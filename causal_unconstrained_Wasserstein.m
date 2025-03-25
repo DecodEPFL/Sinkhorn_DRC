cef79165f9a5f33c6d00181acd60475ce623488c
Riccardo Cescon <cesconriccardo1@gmail.com>
2025-03-25 15:02:48 +0100

    minor changes

--- causal_unconstrained_Wasserstein.m
+++ causal_unconstrained_Wasserstein.m
@@ -1,41 +1,41 @@
 function [Phi_x, Phi_u, objective] = causal_unconstrained_Wasserstein(sys, sls, opt, rho)
-%CAUSAL_UNCONSTRAINED computes an unconstrained causal linear control
+    %CAUSAL_UNCONSTRAINED computes the unconstrained Wasserstein linear
+    %controller
     
     % Define the decision variables of the optimization problem
-    Phi_x = sdpvar(sys.d*opt.N, sys.d*opt.N, 'full');
-    Phi_u = sdpvar(sys.m*opt.N, sys.d*opt.N, 'full');
+    Phi_u = sdpvar(sys.m*opt.N, sys.d + sys.p*(opt.N-1), 'full');
+    Phi_x = (sls.I - sls.Z*sls.A) \ (sls.Z*sls.B*Phi_u + sls.E); % Phi_x is given as function of Phi_u
     Phi = [Phi_x; Phi_u];
-    
+
     s = sdpvar(opt.n, 1);
     gamma = sdpvar();
-    Q = sdpvar(sys.d*opt.N, sys.d*opt.N, 'symmetric');
-
-    constraints = [];
+    Q = sdpvar(sys.d + sys.p*(opt.N-1), sys.d + sys.p*(opt.N-1), 'symmetric');
 
     % define the objective function
     objective = gamma*rho + sum(s)/opt.n;
 
-    % Second LMI constraint
+    % define the constraints
+    constraints = [];
+    
+    % second LMI constraint
     D_half = sqrtm(opt.C);
     LMI2 = [Q, Phi'*D_half'; D_half*Phi, eye((sys.m+sys.d)*opt.N)];
     constraints = [constraints, LMI2 >= 0];
 
-    % Impose the achievability constraints
-    constraints = [constraints, (sls.I - sls.Z*sls.A)*Phi_x - sls.Z*sls.B*Phi_u == sls.E];
     % Impose the causal sparsities on the closed loop responses
     for i = 0:opt.N-2
         for j = i+1:opt.N-1 % Set j from i+2 for non-strictly causal controller (first element in w is x0)
-            constraints = [constraints, Phi_x((1+i*sys.d):((i+1)*sys.d), (1+j*sys.d):((j+1)*sys.d)) == zeros(sys.d, sys.d)];
-            constraints = [constraints, Phi_u((1+i*sys.m):((i+1)*sys.m), (1+j*sys.d):((j+1)*sys.d)) == zeros(sys.m, sys.d)];
+            constraints = [constraints, Phi_u((1+i*sys.m):((i+1)*sys.m), (1+j*sys.p):((j+1)*sys.p)) == zeros(sys.m, sys.p)];
         end
     end
-
+    
+    % Loop for each datapoint 
     for i=1:opt.n
         % Get the i-th datapoint
         xi_hat = opt.data{i};
 
         % First LMI constraint
-        LMI = [gamma*eye(sys.d*opt.N)-Q, gamma*xi_hat; gamma*xi_hat', s(i) + gamma*(xi_hat'*xi_hat)];
+        LMI = [gamma*eye(sys.d + sys.p*(opt.N-1))-Q, gamma*xi_hat; gamma*xi_hat', s(i) + gamma*(xi_hat'*xi_hat)];
         constraints = [constraints, LMI >= 0];
     end
     
--- goldenSearch.m
+++ goldenSearch.m
@@ -1,4 +1,4 @@
-function [cost, lambda_opt, Q_opt, Phi_x, Phi_u] = goldenSearch(rho, eps, tol, lam_min, lam_max, sys, sls, opt, gaussian_meas)
+function [cost, lambda_opt, Q_opt, Phi_x, Phi_u] = goldenSearch(rho, eps, tol, lam_min, lam_max, sys, sls, opt)
     % Taken from https://drlvk.github.io/nm/section-golden-section.html
     % For a reference https://en.wikipedia.org/wiki/Golden-section_search#Iterative_algorithm
 
@@ -7,13 +7,8 @@ function [cost, lambda_opt, Q_opt, Phi_x, Phi_u] = goldenSearch(rho, eps, tol, l
     interval = b - a;
     c = a + gr*interval;
     d = b - gr*interval;
-    if gaussian_meas == true
-        [~,~,~,fc] = causal_unconstrained_gaussian(sys, sls, opt, c, rho, eps);
-        [~,~,~,fd] = causal_unconstrained_gaussian(sys, sls, opt, d, rho, eps);
-    else
-        [~,~,~,fc] = causal_unconstrained(sys, sls, opt, c, rho, eps);
-        [~,~,~,fd] = causal_unconstrained(sys, sls, opt, d, rho, eps);
-    end
+    [~,~,~,fc] = causal_unconstrained_gaussian(sys, sls, opt, c, rho, eps);
+    [~,~,~,fd] = causal_unconstrained_gaussian(sys, sls, opt, d, rho, eps);
     
     while abs(b-a) >= tol
         if fc < fd
@@ -21,21 +16,13 @@ function [cost, lambda_opt, Q_opt, Phi_x, Phi_u] = goldenSearch(rho, eps, tol, l
             d = c;
             fd = fc;
             c = a + gr*(b-a);
-            if gaussian_meas == true
-                [Phi_x, Phi_u, Q, fc] = causal_unconstrained_gaussian(sys, sls, opt, c, rho, eps);
-            else
-                [Phi_x, Phi_u, Q, fc] = causal_unconstrained(sys, sls, opt, c, rho, eps);
-            end
+            [Phi_x, Phi_u, Q, fc] = causal_unconstrained_gaussian(sys, sls, opt, c, rho, eps);
         else
             a = c;
             c = d;
             fc = fd;
             d = b - gr*(b-a);
-            if gaussian_meas == true
-                [Phi_x, Phi_u, Q, fd] = causal_unconstrained_gaussian(sys, sls, opt, d, rho, eps);
-            else
-                [Phi_x, Phi_u, Q, fd] = causal_unconstrained(sys, sls, opt, d, rho, eps);
-            end
+            [Phi_x, Phi_u, Q, fd] = causal_unconstrained_gaussian(sys, sls, opt, d, rho, eps);
         end
     end
     cost = fd;
